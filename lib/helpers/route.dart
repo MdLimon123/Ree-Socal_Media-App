f5cf24f66af550bdd255561edb14b6b479b2c435
Md Limon Islam <limon222019@gmail.com>
2025-09-02 15:24:18 +0600

    project setup

---
+++ lib/helpers/route.dart
@@ -0,0 +1,25 @@
+import 'package:get/get.dart';
+import 'package:ree_social_media_app/views/screen/Home/home_screen.dart';
+import 'package:ree_social_media_app/views/screen/Profile/profile_screen.dart';
+import 'package:ree_social_media_app/views/screen/Wallet/wallet_screen.dart';
+
+
+import '../views/screen/Splash/splash_screen.dart';
+
+class AppRoutes{
+
+  static String splashScreen="/splash_screen";
+  static String homeScreen="/home_screen";
+  static String profileScreen="/profile_screen";
+  static String walletScreen="/wallet_screen";
+
+ static List<GetPage> page=[
+    GetPage(name:splashScreen, page: ()=>const SplashScreen()),
+    GetPage(name:homeScreen, page: ()=>const HomeScreen(),transition:Transition.noTransition),
+    GetPage(name:walletScreen, page: ()=>const WalletScreen(),transition:Transition.noTransition),
+    GetPage(name:profileScreen, page: ()=>const ProfileScreen(),transition: Transition.noTransition),
+  ];
+
+
+
+}
---
+++ lib/views/screen/Splash/splash_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFFABD4A7),
+              Color(0xFFE6E6E6)
+            ]
+          )
+        ),
+    ));
+  }
+}
